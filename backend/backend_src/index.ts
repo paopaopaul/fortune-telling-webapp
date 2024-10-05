5982a357a420211a31e301ef452faca4eba6e396
paopaopaul <lingzishengppl15@gmail.com>
2024-10-05 01:36:24 -0700

    Index.ts request

--- backend/backend_src/index.ts
+++ backend/backend_src/index.ts
@@ -6,7 +6,7 @@ const server = fastify({ logger: true });
 server.register(cors, { origin: "http://localhost:5173", methods:["GET", "POST"],});
 
 //draw lot
-server.get('/api/draw-lot', async (_, reply) => {
+server.get('/api/draw-lot', async (request, reply) => {
   try {
 
     //throw new Error("Simulated server error");
@@ -21,7 +21,7 @@ server.get('/api/draw-lot', async (_, reply) => {
 });
 
 // Global error handler
-server.setErrorHandler((error, _: FastifyRequest, reply: FastifyReply) => {
+server.setErrorHandler((error, request: FastifyRequest, reply: FastifyReply) => {
   server.log.error(error);  // Log the error
 
   // Respond with a generic error message
